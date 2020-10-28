1fc19b4c012979af7440620080056738d252a8e3
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-10-28 09:02:01 -0500

    tried to make a function work on js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+{/* <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Dog</a></li>
+          <li class="breadcrumb-item"><a href="#">Cat</a></li>
+          <li class="breadcrumb-item"><a href="#">Sheep</a></li>
+        </ol>
+</nav> */}
+
+// const breadCrumb = document.createElement('div');
+//     breadCrumb.classList.add('breadcrumb');
+// const navElement = document.add
